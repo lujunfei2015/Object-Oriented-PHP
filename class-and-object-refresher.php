8cdb44edb0fcfcbd179392e5c51ee00d582be1b1
Your Name Here <amyr@amyrhaq.com>
2013-09-26 22:05:37 -0700

    completed lesson 1/8

---
+++ class-and-object-refresher.php
@@ -0,0 +1,20 @@
+<!-- Go ahead and define the Person class, then create a new Person object called $me. -->
+ <html>
+  <head>
+    <title>OOP Review</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // Create a Person class here:
+      class Person
+      {
+      }
+      
+      // And create a Person instance called $me here:
+      $me = new Person();
+      
+      ?>
+    </p>
+  </body>
+</html>
