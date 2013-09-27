55ba9937a3fde1e025418fc156eaf62a73bb6800
Your Name Here <amyr@amyrhaq.com>
2013-09-27 16:49:10 -0700

    completed lesson 4/8

---
+++ overriding-parent-methods.php
@@ -0,0 +1,29 @@
+<!-- We've created a Vehicle class in the editor. 
+Create a child class, Bicycle, that overrides the Vehicle class' honk() method and replaces it with a method that returns "Beep beep!".
+Finally, create a $bicycle instance of the Bicycle class and echo the result of calling its honk() method. 
+Check the Hint for refreshers if you need them! -->
+<html>
+<head>
+    <title>Override!</title>
+</head>
+
+<body>
+    <p>
+    	<?php
+            class Vehicle {
+              public function honk() {
+                return "HONK HONK!";
+              }
+            }
+            // Add your code below!
+            class Bicycle extends Vehicle{
+                public function honk(){
+                    print "Beep beep!";
+                }
+            }
+            $bicycle=new Bicycle();
+            $bicycle->honk();
+        ?>
+    </p>
+</body>
+</html>
